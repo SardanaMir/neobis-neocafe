5b72d102ac2fdaa3237b013e39de75cd4e8a8d4b
SardanaMir <sardanadord@yandex.ru>
2024-02-05 09:00:45 +0300

    T3-30 add logic of login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import logo from '../../assets/img/neocafe-logo.png'
 import bg from '../../assets/img/login-bg.jpg'
+import { useFormik } from 'formik';
 import styles from './styles.module.scss'
 
 const Login = () => {
 
+  const onSubmit = async () =>{
+    console.log(values.email, values.password)
+    try{
+
+    }catch(err){
+      console.log('error')
+    }
+  }
+
+  const {
+    values,
+    isSubmitting,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+} = useFormik({
+    initialValues: {
+    email: "",
+    password: "",
+    },
+    onSubmit,
+  });
   return (
     <div className={styles.root}>
       <div>
@@ -12,10 +35,28 @@ const Login = () => {
         <div className={styles.container}>
           <div className={styles.wrapper}>
             <h1 className={styles.title}>Вход</h1>
-            <form>
-              <input type="email" placeholder='Электронная почта'/>
-              <input type="password" placeholder='Пароль'/>
-              <button className={styles.btn}>Отправить</button>
+            <form onSubmit={handleSubmit}>
+              <input 
+              type="email" 
+              placeholder='Электронная почта' 
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+              id='email'
+              autoComplete='off' 
+              required
+              />
+              <input 
+              type="password" 
+              placeholder='Пароль'
+              onChange={handleChange} 
+              onBlur={handleBlur}
+              value={values.password}
+              autoComplete='off' 
+              id='password'
+              required
+              />
+              <button type="submit" disabled={isSubmitting} className={styles.btn}>Отправить</button>
             </form>
           </div>
         </div>
